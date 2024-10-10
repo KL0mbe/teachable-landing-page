ea1e400e3fcfbf62ed1c17f1f67d77d799c57f84
Metin Özkaya <klombe@outlook.dk>
2024-10-10 16:26:55 +0200

    added title and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Multi Orgasmic Man | Anzaro Mystic Arts',
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
